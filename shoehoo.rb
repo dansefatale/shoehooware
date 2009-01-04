b964deaa0d68e1bc70e1b31c1ae5b8350ebbf55c
Severin Bannert <schreib.mal.sevi@gmail.com>
2009-01-04 21:39:31 +0100

    First commit that works as it should!!!
    I use a new Thread to run the latex code and an
    animate block to watch a indicator variable if
    the latex thread has finished. While this is running
    I display the shoehoo thinking pic and hide the input
    area!

--- shoehoo.rb
+++ shoehoo.rb
@@ -8,56 +8,66 @@ Shoes.app :title => "Mr.Shoehoo does LaTeX", :width => 520, :height => 430, :res
 				@mainback_straight = background "img/Shoehoo_sketch_w500.png"
 				@mainback_think =  background "img/Shoehoo_eyes_hidden_sketch_w500.png"
 				@mainback_think.hide
-				
+
+				@busy = false
+				@ticker = animate(5) do |frame|
+					if !@busy
+						@mainback_straight.show
+						@mainback_think.hide
+						@latexinput.show
+						@ticker.stop
+					else
+						@mainback_straight.hide
+						@mainback_think.show
+						@latexinput.hide
+						@disp.remove
+					end
+				end
+
+	
 				@latexinput = stack do
 
 					@tex = edit_line :width => 250
-					@tex.focus
 					@texbutton = button "Do it!" 
-					@texbutton.click {
-						
-						debug "Will think"
-						@latexinput.hide
-						@mainback_straight.hide
-						@mainback_think.show
-						
-						# determine a filename
-						debug "Thinking"
-						Dir.chdir("/Users/sevi/programming/ruby/shoes/shoetex/img/textest")
-						filebase = "LatexImage1"
-						j = 2
-
-						while File.exists?(filebase + ".png") do 
-							filebase = "LatexImage" + j.to_s
-							j += 1 
-						end 
-						
-						lti = LatexToImage.new(@tex.text ,filebase,"tmpfolder")
-						lti.latexpreamble += "\\usepackage{amsmath}"
-						tic = Time.now
-						lti.process
-						toc = Time.now
-						debug "process call: " + ((toc - tic).to_f).to_s
-
-						# Cleanup
-						File.copy("tmpfolder/" + filebase + ".png", ".")
-						File.delete(*Dir["tmpfolder/*"])
-						Dir.rmdir("tmpfolder") 
-						
-						@mainback_think.hide
-						@mainback_straight.show
-						
-						@disp.remove # = image filebase + ".png"
-						@latexinput.append do
-							@disp = image filebase + ".png" 
-							@disp.style(:height => (@disp.full_height * 1.5).ceil)
-						end
-						@latexinput.show
-						debug "Have thought"
-
-				}
-
 					@disp = para "Type some Latex code"
+
+					@texbutton.click {
+						@busy = true
+						@ticker.start
+						debug "Will think"
+						Thread.new do
+							# determine a filename
+							debug "Thinking in Thread"
+							Dir.chdir("/Users/sevi/programming/ruby/shoes/shoetex/img/textest")
+							filebase = "LatexImage1"
+							j = 2
+
+							while File.exists?(filebase + ".png") do 
+								filebase = "LatexImage" + j.to_s
+								j += 1 
+							end 
+						
+							lti = LatexToImage.new(@tex.text ,filebase,"tmpfolder")
+							lti.latexpreamble += "\\usepackage{amsmath}"
+							tic = Time.now
+							lti.process
+							toc = Time.now
+							debug "process call: " + ((toc - tic).to_f).to_s
+
+							# Cleanup
+							File.copy("tmpfolder/" + filebase + ".png", ".")
+							File.delete(*Dir["tmpfolder/*"])
+							Dir.rmdir("tmpfolder") 
+
+							@latexinput.append do
+								@disp = image filebase + ".png" 
+								@disp.style(:height => (@disp.full_height * 1.5).ceil)
+							end
+							@busy = false
+							debug "Have thought and leave Thread"
+						end
+					}
+
 				end
 
 				@latexinput.move(20,75)
